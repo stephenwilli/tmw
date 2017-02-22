38f43b6a3d627c11b240bd0e2d9666d149aef726
Stephen Williams <stephen@wearetmbr.com>
2017-02-21 22:14:24 -0700

    removed option subpages

--- wp-content/themes/tmw/functions/option-pages.php
+++ wp-content/themes/tmw/functions/option-pages.php
@@ -14,36 +14,36 @@ if( function_exists('acf_add_options_page') ) {
 		'redirect'		=> false // This allows the parent to have it's own page instead of redirecting to the first child.
 	));
 
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Header Settings',
-		'menu_title'	=> 'Header Settings',
-		'parent_slug'	=> 'theme-settings',
-		'capability'	=> 'manage_options'
-	));
+	// acf_add_options_sub_page(array(
+	// 	'page_title' 	=> 'Header Settings',
+	// 	'menu_title'	=> 'Header Settings',
+	// 	'parent_slug'	=> 'theme-settings',
+	// 	'capability'	=> 'manage_options'
+	// ));
 	
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Footer Settings',
-		'menu_title'	=> 'Footer Settings',
-		'parent_slug'	=> 'theme-settings',
-		'capability'	=> 'manage_options'
-	));
+	// acf_add_options_sub_page(array(
+	// 	'page_title' 	=> 'Footer Settings',
+	// 	'menu_title'	=> 'Footer Settings',
+	// 	'parent_slug'	=> 'theme-settings',
+	// 	'capability'	=> 'manage_options'
+	// ));
 
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Tracking Codes',
-		'menu_title'	=> 'Tracking Codes',
-		'parent_slug'	=> 'theme-settings',
-		'capability'	=> 'manage_options'
-	));
+	// acf_add_options_sub_page(array(
+	// 	'page_title' 	=> 'Tracking Codes',
+	// 	'menu_title'	=> 'Tracking Codes',
+	// 	'parent_slug'	=> 'theme-settings',
+	// 	'capability'	=> 'manage_options'
+	// ));
 
 }
 
 // SUB PAGES
 
-function my_acf_options_page_settings( $settings ) {
-	 $settings['title'] = 'Theme Settings';
-	 $settings['pages'] = array('Header', 'Sidebar', 'Footer', 'Tracking');
+// function my_acf_options_page_settings( $settings ) {
+// 	 $settings['title'] = 'Theme Settings';
+// 	 $settings['pages'] = array('Header', 'Sidebar', 'Footer', 'Tracking');
 
-	 return $settings;
-}
+// 	 return $settings;
+// }
 
-add_filter('acf/options_page/settings', 'my_acf_options_page_settings');
+// add_filter('acf/options_page/settings', 'my_acf_options_page_settings');

8847300c755aa86e9972391087e6ae90afba2282
Stephen Williams <stephen@wearetmbr.com>
2017-02-21 22:14:51 -0700

    hero slideshow

--- wp-content/themes/tmw/functions/image-size.php
+++ wp-content/themes/tmw/functions/image-size.php
@@ -4,6 +4,6 @@
 /===================================================== */
 
 if ( function_exists( 'add_image_size' ) ) {
-	add_image_size( 'full_screen', 1680, 920, true ); // Ideal dimensions for load times on full width images
-	add_image_size( 'lg_thumb', 500, 500, true ); // Force crop
+	add_image_size( 'full_screen', 1680, 920, true ); // SLIDER
+	add_image_size( 'lg_thumb', 500, 500, true ); // SQUARE THUMBS
 }
--- wp-content/themes/tmw/partials/home/hero.php
+++ wp-content/themes/tmw/partials/home/hero.php
@@ -1,10 +1,41 @@
-<header>
-    <div class="header-content">
-        <div class="header-content-inner">
-            <h1 id="homeHeading">Your Favorite Source of Free Bootstrap Themes</h1>
-            <hr>
-            <p>Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
-            <a href="#about" class="btn btn-primary btn-xl page-scroll">Find Out More</a>
-        </div>
-    </div>
-</header>
+<div id="home-hero" class="flexslider">
+  <?php if ( get_field( 'hero_slideshow' ) ) { ;?>
+  <ul class="slides">
+
+    <?php while(has_sub_field('hero_slideshow')) {
+
+      $image = get_sub_field('hero_image');
+      $title = get_sub_field('hero_title');
+      $subTitle = get_sub_field('hero_subtitle');
+      $slideLink = get_sub_field('hero_link');
+      ?>
+
+    <li>
+    <?php if ( $slideLink ) { ?>
+        <a href="<?php echo $slideLink; ?>">
+        <?php } ?>
+          
+          <div class="hero-bg" style="background-image : url('<?php echo $image['sizes']['medium'];?>');"></div>
+
+          <div class="hero-caption">
+            <?php if ( $title ) { ?>
+              <h2><?php echo $title; ?></h2>
+            <?php }
+
+            if ( $subTitle ) { ?>
+              <p><?php echo $subTitle; ?></p>
+            <?php } ?>
+          </div><!-- /caption -->
+        
+        <?php if ( $slideLink ) { ?>
+        </a>
+        <?php } ?>
+    
+    </li>
+
+  <?php } ?>
+
+  </ul>
+<?php } ?>
+
+</div><!-- #hero-slider -->

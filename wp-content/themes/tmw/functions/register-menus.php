e7fab583866a5bb275e75a3a731ef6d4db194c58
Stephen Williams <stephen@wearetmbr.com>
2017-03-01 19:35:42 -0700

    menus

--- wp-content/themes/tmw/functions/register-menus.php
+++ wp-content/themes/tmw/functions/register-menus.php
@@ -2,10 +2,11 @@
 
    /* REGISTER MENUS
   /------------------------*/
-  function register_theme_menus() {
-    register_nav_menus([
-      'mainmenu' => __('Main Menu'),
-      'footermenu' => __('Footer Menu')
-    ]);
-  }
-  add_action( 'init', 'register_theme_menus');
+
+add_action('after_setup_theme', 'register_custom_menus');
+
+function register_custom_menus() {
+    register_nav_menus( array(
+        'mainmenu' => __('Main Menu'),
+        'footer_menu' => __('Footer Menu') ) );
+}

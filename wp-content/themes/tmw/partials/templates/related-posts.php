2d331995235cdfd04002b2f19ba6a1264a65cb8d
Stephen Williams <stephen@wearetmbr.com>
2017-03-02 22:12:38 -0700

    single publications

---
+++ wp-content/themes/tmw/partials/templates/related-posts.php
@@ -0,0 +1,32 @@
+<section class="bg-primary related-posts" id="publications">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 text-center">
+                <h2>Related Publications</h2>
+        </div>
+    </div>
+</section>
+
+<section class="nopad">
+    <div class="container-fluid nopad">
+        <div class="row popup-gallery">
+            
+            <?php
+                $args = array(
+                    'post_type' => 'publication',
+                    'order' => 'ASC',
+                    'posts_per_page' => 4
+                );
+                $publications = get_posts( $args );
+                global $post;
+                foreach( $publications as $publication ) {
+                    $post = $publication;
+                    setup_postdata( $post );
+                    get_template_part( 'partials/loops/related-grid' );
+                }
+                wp_reset_postdata();
+            ?>
+                
+        </div>
+    </div>
+</section>
--- wp-content/themes/tmw/single-publication.php
+++ wp-content/themes/tmw/single-publication.php
@@ -2,6 +2,85 @@
 
 	<?php while ( have_posts() ) : the_post(); ?>
 
+		<!--
+
+			title
+			category breadcrumb (button?)
+			
+			2/3 Gallery with slider
+			1/3 text content
+				link
+
+			4x related posts -->
+
+			<article class="publication-wrap">
+				<div class="container">
+
+					<div class="row">
+						<div class="col-sm-12">
+							<h2 class="text-center"><?php the_title(); ?></h2>
+						</div>
+					</div>
+
+					<div class="row">
+						
+						<div class="col-sm-12 col-lg-7">
+
+<!-- 							<?php
+							$images = get_field('image_gallery');
+							if( $images ): ?>
+								<div class="row">
+									<div class="gallery js-project-gallery">
+										<?php
+											foreach( $images as $image ):
+											$full = $image ['sizes']['lg_thumb'];
+											$thumb = $image ['sizes']['lg_thumb'];
+										?>
+											<div class="col-sm-4">
+												<div class="gallery-image">
+													<a href="<?php echo $full ?>">
+														<img class="thumb" src="<?php echo $thumb ?>" />
+													</a>
+												</div>
+											</div>
+										<?php endforeach; ?>
+									</div>
+								</div>
+							<?php endif; ?> -->
+							<?php 
+
+								if ( has_post_thumbnail() ) { 
+								    $image_id = get_post_thumbnail_id();
+								    list( $url, $width, $height ) = wp_get_attachment_image_src( $image_id, 'lg_thumb' );
+
+								?>
+								<div class="gallery">
+									<img src="<?php echo esc_url( $url ); ?>" />
+								</div>
+								<?php } ?>
+						</div>
+
+						<div class="col-sm-12 col-lg-5">
+							<?php the_content();?>
+							<?php 
+								$link = get_field('publication_link');
+								$button = get_field('button_text');
+								if ($link){ ?>
+
+									<p class="text-center"><a class="btn -orange" href="<?php echo $link; ?>" target="_blank"><?php echo $button; ?></a></p>
+
+							<?php } ?>
+						</div>
+
+					</div>
+
+				</div>
+				
+			</article>
+
+			<?php get_template_part('partials/templates/related-posts') ;?>
+
+
 	<?php endwhile; // end of the loop. ?>
 
 <?php get_footer();?>

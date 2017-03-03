01801a8edea675b4cb2a8e410a1944174296edfc
Stephen Williams <stephen@wearetmbr.com>
2017-03-02 20:51:16 -0700

    single templates

--- wp-content/themes/tmw/single-team.php
+++ wp-content/themes/tmw/single-team.php
@@ -2,10 +2,6 @@
 
 	<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php get_template_part( 'partials/loops/single-loop' ); ?>
-
-		<?php the_post_navigation(); ?>
-
 	<?php endwhile; // end of the loop. ?>
 
 <?php get_footer();?>

beca7117f4dda37e3d20294dae81d6a35c4e3eed
Stephen Williams <stephen@wearetmbr.com>
2017-03-02 21:15:59 -0700

    services partial

--- wp-content/themes/tmw/partials/templates/services.php
+++ wp-content/themes/tmw/partials/templates/services.php
@@ -1,27 +1,24 @@
-<section class="full-width-text bg-primary" id="services">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4 col-md-6 text-center">
-                <div class="service-box">
-                    <img src="/wp-content/uploads/2017/02/news-icon.png"/>
-                    <h3>65,000</h3>
-                    <p>Newspapers read each week, distrubuted across 750 locations</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 text-center">
-                <div class="service-box">
-                    <img src="/wp-content/uploads/2017/02/magazine-icon-01.png"/>
-                    <h3>100,000</h3>
-                    <p>Magazines each year distributed across hotels retailers and other key locations</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 text-center">
-                <div class="service-box">
-                    <img src="/wp-content/uploads/2017/02/hands-icon.png"/>
-                    <h3>1,300</h3>
-                    <p>Sucessfull advertising parners each year</p>
-                </div>
+<?php
+    if ( have_rows('service') ):
+?>
+    <section class="full-width-text bg-primary" id="services">
+        <div class="container">
+            <div class="row">
+        
+                <?php while( have_rows('service') ): the_row(); 
+                    $icon = get_sub_field('service_icon');
+                    $title = get_sub_field('service_title');
+                    $text = get_sub_field('service_text');
+                ?>
+                    <div class="col-lg-4 col-md-6 text-center">
+                        <div class="service-box">
+                            <img src="<?php echo $icon['sizes']['medium'] ;?>"/>
+                            <h3><?php echo $title; ?></h3>
+                            <p><?php echo $text; ?></p>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>

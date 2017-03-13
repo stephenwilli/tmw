5c08a6890c7896b3c62f8c3a373747008af3452d
Stephen Williams <stephen@wearetmbr.com>
2017-03-12 18:50:27 -0600

    slider

--- wp-content/themes/tmw/partials/templates/hero.php
+++ wp-content/themes/tmw/partials/templates/hero.php
@@ -1,44 +1,58 @@
-<div id="hero-slider" class="flexslider">
-  <?php if ( get_field( 'hero_slideshow' ) ) { ;?>
-  
-  <ul class="slides">
+<?php 
+  $option = get_field('hero_option'); 
+  if ($option == 'slider') { ?>
 
-    <?php while(has_sub_field('hero_slideshow')) {
+  <div id="hero-slider" class="flexslider">
+    <?php if ( get_field( 'hero_slideshow' ) ) { ;?>
+    
+      <ul class="slides">
 
-      $image = get_sub_field('hero_image');
-      $title = get_sub_field('hero_title');
-      $subTitle = get_sub_field('hero_subtitle');
-      $buttonText = get_sub_field('hero_button_text');
-      $slideLink = get_sub_field('hero_link');
-      $slideAlign = get_sub_field('hero_alignment');
-      ?>
-
-    <li>
-
-      <div class="hero-bg parallax" style="background-image : url('<?php echo $image['sizes']['full_screen'];?>');"></div>
-
-      <?php if ( $title ) { ?>
-        <div class="hero-caption caption-<?php echo $slideAlign; ?>">
+        <?php while(has_sub_field('hero_slideshow')) {
           
-            <h2><mark><?php echo $title; ?></mark></h2>
-          
-            <?php if ( $subTitle ) { ?>
-              <p><?php echo $subTitle; ?></p>
-            <?php } ?>
+          $image = get_sub_field('hero_image');
+          $title = get_sub_field('hero_title');
+          $subTitle = get_sub_field('hero_subtitle');
+          $buttonText = get_sub_field('hero_button_text');
+          $slideLink = get_sub_field('hero_link');
+          $slideAlign = get_sub_field('hero_alignment');
+          ?>
 
-            <?php if ( $buttonText ) { ?>
-              <a href="<?php echo $slideLink; ?>" class="btn -orange"><?php echo $buttonText; ?></a>
-            <?php } ?>
+        <li>
 
-        </div><!-- /caption -->
+          <div class="hero-bg parallax" style="background-image : url('<?php echo $image['sizes']['full_screen'];?>');"></div>
+
+          <?php if ( $title ) { ?>
+            <div class="hero-caption caption-<?php echo $slideAlign; ?>">
+              
+                <h2><mark><?php echo $title; ?></mark></h2>
+              
+                <?php if ( $subTitle ) { ?>
+                  <p><?php echo $subTitle; ?></p>
+                <?php } ?>
+
+                <?php if ( $buttonText ) { ?>
+                  <a href="<?php echo $slideLink; ?>" class="btn -orange"><?php echo $buttonText; ?></a>
+                <?php } ?>
+
+            </div><!-- /caption -->
+          <?php } ?>
+      
+        </li>
+
       <?php } ?>
-  
-    </li>
 
-  <?php } ?>
+      </ul>
 
-  </ul>
+    <?php } ?>
+
+  </div><!-- #hero-slider -->
+
+<?php } else { 
+  $singleHero = get_field('hero_single');
+  ?>
+
+<div id="single-hero" class="flexslider">
+  <div class="hero-bg parallax" style="background-image : url('<?php echo $singleHero['sizes']['full_screen'];?>');"></div>
+</div>
 
 <?php } ?>
-
-</div><!-- #hero-slider -->

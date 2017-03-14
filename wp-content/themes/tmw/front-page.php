67552de82e3dbf31eb93459968fcfcc3fba8a0bf
Stephen Williams <stephen@wearetmbr.com>
2017-03-13 22:54:22 -0600

    homepage template

--- wp-content/themes/tmw/front-page.php
+++ wp-content/themes/tmw/front-page.php
@@ -1,17 +1,13 @@
-<?php get_header();?>
+<?php get_header();
 
-<?php get_template_part('partials/templates/hero'); ?>
+// HOME PAGE TEMPLATE
 
-<?php get_template_part('partials/home/intro-text'); ?>
+?>
 
-<!-- <?php get_template_part('partials/home/spin'); ?>
- -->
-<?php get_template_part('partials/templates/full-width-image'); ?>
+<?php get_template_part('partials/content/hero'); ?>
 
-<?php get_template_part('partials/home/grid'); ?>
+<!-- FLEXIBLE CONTENT TEMPLATES -->
 
-<?php get_template_part('partials/home/left-caption'); ?>
-
-<?php get_template_part('partials/home/right-caption'); ?>
+<?php get_template_part('partials/content/flex-templates'); ?>
 
 <?php get_footer();?>

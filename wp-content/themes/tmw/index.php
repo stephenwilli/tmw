d8644173e06911b7f75ff61214d955f02a791310
Stephen Williams <stephen@wearetmbr.com>
2017-03-13 22:39:48 -0600

    default page template

--- wp-content/themes/tmw/index.php
+++ wp-content/themes/tmw/index.php
@@ -1,1 +1,16 @@
-<? get_template_part('templates/wp', 'page'); ?>
+<?php
+
+?>
+<?php get_header(); ?>
+
+	<!-- PAGE HERO -->
+
+	<?php get_template_part('partials/content/hero'); ?>
+
+	<!-- WYSIWYG CONTENT -->
+
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php get_template_part('partials/global/page-content'); ?>
+	<?php endwhile; // end of the loop. ?>
+
+<?php get_footer();?>

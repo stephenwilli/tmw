354737d70d9fb8724bcb79da884de4eae06e6a03
Stephen Williams <stephen@wearetmbr.com>
2017-03-27 22:36:50 -0600

    js count

--- wp-content/themes/tmw/assets/scripts/main.js
+++ wp-content/themes/tmw/assets/scripts/main.js
@@ -1,5 +1,9 @@
 (function ($) {
 
+  // WOW ANIMATIONS
+
+  new WOW().init();
+
 	// ADD CLASS TO NAV ON SCROLL
 
 	$(window).scroll(function() {
@@ -12,87 +16,111 @@
 
 	}); // window.scroll
 
-    // HERO SLIDER
+  // HERO SLIDER
       
-      $('#hero-slider').flexslider({
-        animation: "fade",
-        slideshow: true, // auto play on load
-        slideshowSpeed: 5000,
-            animationSpeed: 800,
-            pauseOnHover: true,
-            directionNav: true, //Boolean: Create navigation for previous/next navigation? (true/false)
-            controlNav: true, //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
-            prevText: "",
-            nextText: ""
-      });
+    $('#hero-slider').flexslider({
+      animation: "fade",
+      slideshow: true, // auto play on load
+      slideshowSpeed: 5000,
+          animationSpeed: 800,
+          pauseOnHover: true,
+          directionNav: false, //Boolean: Create navigation for previous/next navigation? (true/false)
+          controlNav: false, //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
+          prevText: "",
+          nextText: ""
+    });
 
-    // PUBLICATION SLIDER
+  // PUBLICATION SLIDER
 
-    $('#js-image-carousel').flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: false,
-        slideshow: false,
-        itemWidth: 215,
-        itemMargin: 10,
-        asNavFor: '#js-image-slider',
-        prevText: "",
-        nextText: ""
-      });
-     
-      $('#js-image-slider').flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: false,
-        slideshow: false,
-        sync: "#js-image-carousel",
-        prevText: "",
-        nextText: ""
-      });
+  $('#js-image-carousel').flexslider({
+      animation: "slide",
+      controlNav: false,
+      animationLoop: false,
+      slideshow: false,
+      itemWidth: 215,
+      itemMargin: 10,
+      asNavFor: '#js-image-slider',
+      prevText: "",
+      nextText: ""
+    });
+   
+    $('#js-image-slider').flexslider({
+      animation: "slide",
+      controlNav: false,
+      animationLoop: false,
+      slideshow: false,
+      sync: "#js-image-carousel",
+      prevText: "",
+      nextText: ""
+    });
 
-    // PRELOADER
+  // PRELOADER
 
 	$(window).load(function(){
 		$('#preloader').fadeOut('slow',function(){$(this).remove();});
 	});
 
+  // ISOTOPE GRID
+
   $(window).load(function(){
 
-    // external js: isotope.pkgd.js
-
-        // init Isotope
-        var $grid = $('.project-grid').isotope({
-          itemSelector: '.project-item',
-          layoutMode: 'masonry'
-        });
-        // filter functions
-        var filterFns = {
-          // show if number is greater than 50
-          numberGreaterThan50: function() {
-            var number = $(this).find('.number').text();
-            return parseInt( number, 10 ) > 50;
-          },
-          // show if name ends with -ium
-          ium: function() {
-            var name = $(this).find('.name').text();
-            return name.match( /ium$/ );
-          }
-        };
-        // bind filter button click
-        $('.filters-button-group').on( 'click', 'a', function() {
-          var filterValue = $( this ).attr('data-filter');
-          // use filterFn if matches value
-          filterValue = filterFns[ filterValue ] || filterValue;
-          $grid.isotope({ filter: filterValue });
-        });
-        // change is-checked class on buttons
-        $('.button-group').each( function( i, buttonGroup ) {
-          var $buttonGroup = $( buttonGroup );
-          $buttonGroup.on( 'click', 'a', function() {
-            $buttonGroup.find('.active').removeClass('active');
-            $( this ).addClass('active');
-          });
-        });
+    // init Isotope
+    var $grid = $('.project-grid').isotope({
+      itemSelector: '.project-item',
+      layoutMode: 'masonry'
+    });
+    // filter functions
+    var filterFns = {
+      // show if number is greater than 50
+      numberGreaterThan50: function() {
+        var number = $(this).find('.number').text();
+        return parseInt( number, 10 ) > 50;
+      },
+      // show if name ends with -ium
+      ium: function() {
+        var name = $(this).find('.name').text();
+        return name.match( /ium$/ );
+      }
+    };
+    // bind filter button click
+    $('.filters-button-group').on( 'click', 'a', function() {
+      var filterValue = $( this ).attr('data-filter');
+      // use filterFn if matches value
+      filterValue = filterFns[ filterValue ] || filterValue;
+      $grid.isotope({ filter: filterValue });
+    });
+    // change is-checked class on buttons
+    $('.button-group').each( function( i, buttonGroup ) {
+      var $buttonGroup = $( buttonGroup );
+      $buttonGroup.on( 'click', 'a', function() {
+        $buttonGroup.find('.active').removeClass('active');
+        $( this ).addClass('active');
+      });
+    });
   });
 
-})(jQuery); // End of use strict
+  // JQUERY COUNT UP
+
+  $('.counter').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+
+    {
+      duration: 4000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+      }
+
+    });  
+
+  });
+
+})(jQuery);
--- wp-content/themes/tmw/partials/content/three-icons.php
+++ wp-content/themes/tmw/partials/content/three-icons.php
@@ -10,12 +10,13 @@
                 <?php while( have_rows('ti_icon_block') ): the_row(); 
                     $TIicon = get_sub_field('ti_icon');
                     $TItitle = get_sub_field('ti_icon_title');
+                    settype($TItitle, "integer");
                     $TItext = get_sub_field('ti_icon_text');
                 ?>
                     <div class="col-sm-4 text-center">
                         <div class="icon-box">
                             <img src="<?php echo $TIicon['sizes']['medium'] ;?>"/>
-                            <h3><?php echo $TItitle; ?></h3>
+                            <h3 class="counter" data-count="<?php echo $TItitle; ?>">0<span>+</span></h3>
                             <p><?php echo $TItext; ?></p>
                         </div>
                     </div>

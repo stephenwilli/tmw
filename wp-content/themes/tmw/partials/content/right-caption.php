f3fe7be856a0b7c322a58bd50519b77c6036ac0e
Stephen Williams <stephen@wearetmbr.com>
2017-03-30 20:01:08 -0600

    right caption template

--- wp-content/themes/tmw/partials/content/right-caption.php
+++ wp-content/themes/tmw/partials/content/right-caption.php
@@ -2,18 +2,17 @@
 
 	// RIGHT CAPTION FLEXIBLE CONTENT LAYOUT RC
 	
-    $RCmediaType = get_sub_field('rc_media_type');
-	$RCsingleImage = get_sub_field('rc_single_image');
-    $RCtopImage = get_sub_field('rc_top_image');
-    $RCbottomImage = get_sub_field('rc_bottom_image');
-    $RCvideo = get_sub_field('rc_video_embed');
-    $RCcaptionType = get_sub_field('rc_caption_type');
-    $RCcaptionTitle = get_sub_field('rc_caption_title');
-    $RCcaptionText = get_sub_field('rc_caption_text');
-    $RCcaptionIcon = get_sub_field('rc_caption_icon');
-    $RCcaptionButton = get_sub_field('rc_caption_button');
-    $RCcaptionLink = get_sub_field('rc_caption_link');
-    $RCwysiwyg = get_sub_field('rc_wysiwyg');
+    $RCmediaType = get_sub_field('right_media_type');
+	$RCsingleImage = get_sub_field('right_single_image');
+    $RCtopImage = get_sub_field('right_top_image');
+    $RCbottomImage = get_sub_field('right_bottom_image');
+    $RCcaptionType = get_sub_field('right_caption_type');
+    $RCcaptionTitle = get_sub_field('right_caption_title');
+    $RCcaptionText = get_sub_field('right_caption_text');
+    $RCcaptionIcon = get_sub_field('right_caption_icon');
+    $RCcaptionButton = get_sub_field('right_caption_button');
+    $RCcaptionLink = get_sub_field('right_caption_link');
+    $RCwysiwyg = get_sub_field('right_wysiwyg');
 ?>
 
 
@@ -23,19 +22,13 @@
         <div class="row">
             <div class="col-sm-12 col-md-7 wow fadeInLeft">
 
-                <?php if ($RCmediaType = 'single_image') { ?>
+                <?php if ($RCmediaType == 'single') { ?>
 	                <img class="single" src="<?php echo $RCsingleImage['sizes']['large'];?>">
                 <?php } ?>
             	
-            	<?php if ($RCmediaType = 'double_image') { ?>
+            	<?php if ($RCmediaType == 'double') { ?>
 	                <img class="top" src="<?php echo $RCtopImage['sizes']['large'];?>">
 	                <img class="bottom" src="<?php echo $RCbottomImage['sizes']['large'];?>">
-                <?php } ?>
-
-                <?php if ($RCmediaType = 'video') { ?>
-	                <div class="video">
-	                	<?php echo $RCvideo; ?>
-	                </div>
                 <?php } ?>
 
             </div>

77ad6e679ba32466db8091a2bec23b27ad115537
Stephen Williams <stephen@wearetmbr.com>
2017-03-30 20:13:01 -0600

    left caption template

--- wp-content/themes/tmw/partials/content/left-caption.php
+++ wp-content/themes/tmw/partials/content/left-caption.php
@@ -2,18 +2,17 @@
 
 	// RIGHT CAPTION FLEXIBLE CONTENT LAYOUT LC
 
-    $LCmediaType = get_sub_field('lc_media_type');
-	$LCsingleImage = get_sub_field('lc_single_image');
-    $LCtopImage = get_sub_field('lc_top_image');
-    $LCbottomImage = get_sub_field('lc_bottom_image');
-    $LCvideo = get_sub_field('lc_video_embed');
-    $LCcaptionType = get_sub_field('lc_caption_type');
-    $LCcaptionTitle = get_sub_field('lc_caption_title');
-    $LCcaptionText = get_sub_field('lc_caption_text');
-    $LCcaptionIcon = get_sub_field('lc_caption_icon');
-    $LCcaptionButton = get_sub_field('lc_caption_button');
-    $LCcaptionLink = get_sub_field('lc_caption_link');
-    $LCwysiwyg = get_sub_field('lc_wysiwyg');
+    $LCmediaType = get_sub_field('left_media_type');
+	$LCsingleImage = get_sub_field('left_single_image');
+    $LCtopImage = get_sub_field('left_top_image');
+    $LCbottomImage = get_sub_field('left_bottom_image');
+    $LCcaptionType = get_sub_field('left_caption_type');
+    $LCcaptionTitle = get_sub_field('left_caption_title');
+    $LCcaptionText = get_sub_field('left_caption_text');
+    $LCcaptionIcon = get_sub_field('left_caption_icon');
+    $LCcaptionButton = get_sub_field('left_caption_button');
+    $LCcaptionLink = get_sub_field('left_caption_link');
+    $LCwysiwyg = get_sub_field('left_wysiwyg');
 
 ?>
 
@@ -55,19 +54,13 @@
 
             <div class="col-sm-12 col-md-7 wow fadeInRight">
 
-                <?php if ($LCmediaType = 'single_image') { ?>
+                <?php if ($LCmediaType == 'single') { ?>
 	                <img class="single" src="<?php echo $LCsingleImage['sizes']['large'];?>">
                 <?php } ?>
             	
-            	<?php if ($LCmediaType = 'double_image') { ?>
+            	<?php if ($LCmediaType == 'double') { ?>
 	                <img class="top" src="<?php echo $LCtopImage['sizes']['large'];?>">
 	                <img class="bottom" src="<?php echo $LCbottomImage['sizes']['large'];?>">
-                <?php } ?>
-
-                <?php if ($LCmediaType = 'video') { ?>
-	                <div class="video">
-	                	<?php echo $LCvideo; ?>
-	                </div>
                 <?php } ?>
 
             </div>

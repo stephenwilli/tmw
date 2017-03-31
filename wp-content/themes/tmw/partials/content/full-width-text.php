c9ef097805fdd5e9658f47682e834f71f541b8ed
Stephen Williams <stephen@wearetmbr.com>
2017-03-30 20:44:57 -0600

    styles

--- wp-content/themes/tmw/partials/content/full-width-text.php
+++ wp-content/themes/tmw/partials/content/full-width-text.php
@@ -6,16 +6,14 @@
     $FWTtext = get_sub_field('fwt_text');
     $FWTwysiwyg = get_sub_field('fwt_wysiwyg');        
 ?>
-    <section class="icon-wrap bg-secondary">
+    <section class="full-text bg-secondary">
         <div class="container">
             <div class="row">
 
-                <div class="col-lg-8 col-lg-offset-2 text-center wow fadeInUp">
-                    <div class="full-width-text">
-                        <h2><?php echo $FWTtitle; ?></h3>
-                        <p><?php echo $FWTtext; ?></p>
-                        <?php echo $FWTwysiwyg; ?>
-                    </div>
+                <div class="col-lg-8 col-lg-offset-2 col-md-12 text-center wow fadeInUp">
+                    <h2><?php echo $FWTtitle; ?></h3>
+                    <p><?php echo $FWTtext; ?></p>
+                    <?php echo $FWTwysiwyg; ?>
                 </div>
            
             </div>

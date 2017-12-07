d4ce1265222989163edc50146d56f306db81c821
Stephen Williams <stephen@wearetmbr.com>
2017-12-07 16:33:31 -0600

    related posts

--- wp-content/themes/tmw/partials/content/related-posts.php
+++ wp-content/themes/tmw/partials/content/related-posts.php
@@ -1,8 +1,9 @@
-<section class="bg-primary related-posts" id="publications">
+<section class="bg-secondary related-posts" id="publications">
     <div class="container">
         <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 text-center">
-                <h2>Related Publications</h2>
+          <div class="col-lg-8 col-lg-offset-2 text-center">
+              <h2>Related Publications</h2>
+          </div>
         </div>
     </div>
 </section>
@@ -12,8 +13,11 @@
         <div class="row wow fadeInUp">
             
             <?php
+                $category = get_field('publication_category');
                 $args = array(
                     'post_type' => 'publication',
+                    'meta_key'		=> 'publication_category',
+	                  'meta_value'	=> $category,
                     'order' => 'ASC',
                     'posts_per_page' => 4
                 );
